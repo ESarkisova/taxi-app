0d019a5ac9531372bcf8110b0291fa56de3d73bb
sarkisova_ea <sarkisova_ea@sptnk.co>
2020-10-18 05:09:12 +0400

    Updates

---
+++ precache-manifest.0a22b65cb03a589dd3ae94970f92e1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d026f1731b2d0632832307ff499a7d",
+    "url": "/taxi-app/index.html"
+  },
+  {
+    "revision": "c652bdc8b3368749b8bb",
+    "url": "/taxi-app/static/css/main.044f4d72.chunk.css"
+  },
+  {
+    "revision": "f6fce151e503ceeb3562",
+    "url": "/taxi-app/static/js/2.653678ff.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/taxi-app/static/js/2.653678ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c652bdc8b3368749b8bb",
+    "url": "/taxi-app/static/js/main.55c2b9cc.chunk.js"
+  },
+  {
+    "revision": "7b802f7f85dd7f7f920d",
+    "url": "/taxi-app/static/js/runtime-main.d7c6e21b.js"
+  }
+]);
